9289e9b594a882456c74a5d096bb3c8a7700f4fc
z-hawa <83214880+z-hawa@users.noreply.github.com>
2021-07-20 00:34:03 +0530

    Update test cases

--- test_session10.py
+++ test_session10.py
@@ -1,12 +1,16 @@
+from ast import Index
 import pytest
 import re
+from session10_special import *
 from session10 import *
 import inspect
 import session10
 import os
 import test_session10
 
-README_CONTENT_CHECK_FOR=['session10','Sequence','vertices','edges','properties','max','ratio']
+README_CONTENT_CHECK_FOR = ['session10', 'Sequence', 'vertices', 'edges', 'properties', 'max', 'ratio']
+
+
 def test_readme_exists():
     assert os.path.isfile(
         "README.md"), "README.md file missing!"
@@ -39,6 +43,7 @@ def test_readme_file_for_formatting():
     f.close()
     assert content.count("#") >= 10
 
+
 def test_function_repeatations():
     functions = inspect.getmembers(test_session10, inspect.isfunction)
     names = []
@@ -53,5 +58,84 @@ def test_function_doc_string():
     """
     functions = inspect.getmembers(session10, inspect.isfunction)
     for function in functions:
-            assert function[1].__doc__
+        assert function[1].__doc__
 
+
+def test_normal_polygon_properties():
+    polygon = RegPolygon(4, 5)
+    assert polygon.area == 0.5 * 4 * 5 * math.cos(math.pi / 4) * 2 * 5 * math.sin(math.pi / 4)
+    assert polygon.edges == 4
+    assert polygon.perimeter == 4 * 2 * 5 * math.sin(math.pi / 4)
+    assert polygon.interiorangle == (4 - 2) * 180 / 4
+    assert polygon.edgelength == 2 * 5 * math.sin(math.pi / 4)
+    assert polygon.apothem == 5 * math.cos(math.pi / 4)
+    assert polygon.perimeter == 4 * 2 * 5 * math.sin(math.pi / 4)
+
+
+def test_polygon_with_illegal_sides():
+    with pytest.raises(ValueError):
+        RegPolygon(2, 3)
+
+
+def test_polygon_with_illegal_data_type():
+    with pytest.raises(TypeError):
+        RegPolygon("hmm", "ee")
+
+
+def test_polygon_greater_than():
+    assert RegPolygon(4, 2) > RegPolygon(3, 1)
+
+
+def test_polygon_equals_to():
+    assert RegPolygon(4, 2) == RegPolygon(4, 2)
+
+
+def test_polygon_repr():
+    temp = RegPolygon(4, 4)
+    assert len(temp.__repr__()) > 25
+
+
+# Special Polygon tests
+
+def test_special_polygon_properties():
+    SpPolygon = SpecialPolygon(3, 1)
+    assert SpPolygon.Circum == 1
+    assert SpPolygon.MaxVertice == 3
+
+
+def test_special_polygon_with_illegal_sides():
+    with pytest.raises(ValueError):
+        SpecialPolygon(2, 3)
+
+
+def test_special_polygon_with_illegal_data_type():
+    with pytest.raises(TypeError):
+        SpecialPolygon("hmm", "ee")
+
+
+def test_special_polygon_length():
+    temp=SpecialPolygon(3,5)
+    assert temp.__len__()==1
+
+
+def test_special_polygon_max_ratio():
+    main=SpecialPolygon(5,1)
+    temp1=RegPolygon(3,1)
+    temp2=RegPolygon(4,1)
+    temp3=RegPolygon(5,1)
+    ls=[temp1.area/temp1.perimeter,temp2.area/temp2.perimeter,temp3.area/temp3.perimeter]
+    assert max(ls)==main.highest_ratios()
+
+def test_special_polygon_repr():
+    temp = SpecialPolygon(4, 4)
+    assert len(temp.__repr__()) > 25
+
+def test_special_polygon_getitem_():
+    temp=SpecialPolygon(8,2)
+    assert len(temp[2:4])==2
+
+def test_special_polygon_getitem_illegal():
+    temp=SpecialPolygon(3,4)
+    with pytest.raises(IndexError):
+        temp[1]
+  
